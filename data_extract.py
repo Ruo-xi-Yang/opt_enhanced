e670b1a50be5b676ab063266102dcf72844783ea
Ruo-xi-Yang <149615117+Ruo-xi-Yang@users.noreply.github.com>
2024-05-01 17:35:24 +0100

    Update data_extract.py

--- data_extract.py
+++ data_extract.py
@@ -1,53 +1,75 @@
 import os
 import csv
+import pandas as pd
 
 def data_extract(wdr):
     idv_files = [filename for filename in os.listdir(wdr) if filename.startswith('idv')]
-    x = []
-    y = []
-    cl = []
-    CD = []
+    data = []
 
     for idv_file in idv_files:
         idv_path = os.path.join(wdr, idv_file)
-        # Assuming importDVs and importclcd functions read data from CSV files
-        dv_x, dv_y, clcd = read_data_from_csv_files(idv_path)
-        cl.append(clcd[0][0])
-        CD.append(clcd[0][1])
-        x.append(dv_x[0])
-        y.append(dv_y[0])
+        clcd, dv = read_data_from_csv_files(idv_path)
+        data.append([dv[0], dv[1], clcd[0], clcd[1]])
 
-    return x, y, cl, CD
+    return pd.DataFrame(data, columns=['x', 'y', 'cl', 'CD'])
 
 # Implement the read_data_from_csv_files function to read data from CSV files
 def read_data_from_csv_files(idv_path):
-    dv_x = []
-    dv_y = []
+    dv = []
     clcd = []
-    # Read data from 'DVs.csv' and 'cl_cd_idv.csv' files
+    # Read data from 'DVs.csv' file
     with open(os.path.join(idv_path, 'DVs.csv'), 'r') as dv_file:
         dv_reader = csv.reader(dv_file)
         dv_data = list(dv_reader)
-        dv_x.append(float(dv_data[0][0]))
-        dv_y.append(float(dv_data[0][1]))
+        dv.append(float(dv_data[0][0]))  # Assuming the first column contains x values
+        dv.append(float(dv_data[0][1]))  # Assuming the second column contains y values
 
+    # Read data from 'cl_cd_idv.csv' file
     with open(os.path.join(idv_path, 'cl_cd_idv.csv'), 'r') as clcd_file:
         clcd_reader = csv.reader(clcd_file)
         clcd_data = list(clcd_reader)
-        clcd.append((float(clcd_data[0][0]), float(clcd_data[0][1])))
+        clcd.append(float(clcd_data[0][0]))  # Assuming the first cell contains cl value
+        clcd.append(float(clcd_data[0][1]))  # Assuming the second cell contains CD value
 
-    return dv_x, dv_y, clcd
+    return clcd, dv
 
-x = [None] * 10
-y = [None] * 10
-cl = [None] * 10
-cd = [None] * 10
-pf = [None] * 10
+# Main code
 ngen = 2
 neval = 30 + 15 * (ngen - 1)
+output_directory = 'C:\\Users\\bzdell\\Desktop\\FYP3004'
 
-# read the folders and extract x y cl cd
+gen_data = {}
 for i in range(1, ngen + 1):
     gen = 'gen_{}'.format(i)
-    wdr = os.path.join('C:\\Users\\bzdell\\Desktop\\FYP3004', gen)
-    x[i - 1], y[i - 1], cl[i - 1], cd[i - 1] = data_extract(wdr)
+    wdr = os.path.join(output_directory, gen)
+    gen_data[gen] = data_extract(wdr)
+    
+    # Output x, y, cl, CD of each generation to CSV
+    gen_csv_filename = os.path.join(output_directory, 'generation_{}.csv'.format(i))
+    gen_data[gen].to_csv(gen_csv_filename, index=False)
+    print("CSV file for {} generation saved as '{}'.".format(gen, gen_csv_filename))
+
+# Combine all x, y, cl, and CD values into arrays
+combined_cl = []
+combined_x = []
+combined_y = []
+combined_CD = []
+
+for gen, df in gen_data.items():
+    combined_x.extend(df['x'].tolist())
+    combined_y.extend(df['y'].tolist())
+    combined_cl.extend(df['cl'].tolist())
+    combined_CD.extend(df['CD'].tolist())
+
+# Create a DataFrame from the combined data
+combined_df = pd.DataFrame({
+    'x': combined_x,
+    'y': combined_y,
+    'cl': combined_cl,
+    'CD': combined_CD
+})
+
+# Output the combined DataFrame to CSV
+combined_csv_filename = os.path.join(output_directory, 'combined_data.csv')
+combined_df.to_csv(combined_csv_filename, index=False)
+print("Combined DataFrame saved as '{}'.".format(combined_csv_filename))
